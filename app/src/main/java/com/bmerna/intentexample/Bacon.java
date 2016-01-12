c7dbb6fcf5d1fb40b8fa3a14958cefcc90483649
Brendan Merna <bmernax30@gmail.com>
2016-01-12 09:26:43 -0500

    Initial commit

---
+++ app/src/main/java/com/bmerna/intentexample/Bacon.java
@@ -0,0 +1,32 @@
+package com.bmerna.intentexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class Bacon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacon);
+
+        Bundle applesData = getIntent().getExtras();
+        if(applesData==null){
+            return;
+        }
+        String appleMessage = applesData.getString("appleMessage");
+        final TextView baconText = (TextView) findViewById(R.id.bacon_text);
+        baconText.setText(appleMessage);
+    }
+
+    public void onClick(View v){
+        Intent i = new Intent(this,Apples.class);
+        startActivity(i);
+    }
+}
